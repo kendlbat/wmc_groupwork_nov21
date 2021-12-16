4471b4eb2404ae9f784a51634571c347fd3c13b6
Noah Arsić <noah.arsic@gmx.at>
2021-12-16 19:42:10 +0100

    Finished if-else.html
    Finished the if-else.html file, added some .js-file as examples and made some edits in the css-file

---
+++ examples/t04/04.js
@@ -0,0 +1,17 @@
+var foo = 3;
+switch(foo) {
+    case 1:
+        console.log("foo = 1");
+        break;
+    case 2:
+        console.log("foo = 2");
+        break;
+    case 3:
+        console.log("foo = 3");
+        break;
+    case 4:
+        console.log("foo = 4");
+        break;
+    default:
+        console.log("foo ist unbestimmbar");
+}
---
+++ examples/t04/05.js
@@ -0,0 +1,14 @@
+var foo = 2;
+switch(foo) {
+    case 1:
+        console.log("foo = 1");
+    case 2:
+        console.log("foo = 2");
+    case 3:
+        console.log("foo = 3");
+        break;
+    case 4:
+        console.log("foo = 4");
+    default:
+        console.log("foo ist unbestimmbar");
+}

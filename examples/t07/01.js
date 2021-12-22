a8ed2610369ac80e1f54f89e45498a41f1250e18
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2021-12-22 12:07:56 +0100

    Added new examples to list

--- examples/t07/01.js
+++ examples/t07/01.js
@@ -1,5 +1,5 @@
 function myFunction(var1, var2) {
-    return var1 + var2; //Die Funktion gibt die Summe von var1 und var2 zurück.
+    return var1 + var2;  // Die Funktion gibt die Summe von var1 und var2 zurück.
 }
 
 console.log(myFunction(2, 3));
